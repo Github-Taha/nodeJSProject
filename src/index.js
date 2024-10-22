13866dc58f5a7a705bd1d2a20b0836a46c986fc6
MasterRyped <136719697+Github-Taha@users.noreply.github.com>
2024-10-22 18:46:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const filename = 'text.txt';
-const hostname = '99.255.96.114';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
